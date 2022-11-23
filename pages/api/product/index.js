550516986a1808b26e47bac03b2cfdd3b398baf9
Kerim Can Gökdağ <kgokdagg@gmail.com>
2022-11-23 21:53:40 +0300

    added product link to category,updated wrong name variable and css

--- pages/api/product/index.js
+++ pages/api/product/index.js
@@ -5,12 +5,12 @@ export default function handler(req, res) {
     const productId = req.body.productId
     // console.log("productId", productId)
     // console.log("req.params ::: ",req.body)
-    let furkan = []
+    let productsArray = []
     productId.map((x) => {
         let foundedProduct = mockData.items.find((y) => y.productId==x )
         // console.log("founded::::",foundedProduct)
-        if(foundedProduct) furkan.push(foundedProduct)
+        if(foundedProduct) productsArray.push(foundedProduct)
     })
    
-     res.json({favoriteProducts:furkan})
+     res.json({favoriteProducts:productsArray})
 }
--- redux/services/productApi.js
+++ redux/services/productApi.js
@@ -3,21 +3,19 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 export const productApi = createApi({
     reducerPath: 'productApi',
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/"}),
+    baseQuery: fetchBaseQuery({baseUrl: "http://localhost:3000/"}),
     endpoints: (builder) => ({
-      getProduct: builder.query({
-        query: (arg) =>{
-          console.log("arg", arg)
-            
-      return{
-                url:`api/product`,
-                body: {productId:arg},
-                method:"POST"
+        getProduct: builder.query({
+            query: (arg) => {
+                return {
+                    url: `api/product`,
+                    body: {productId: arg},
+                    method: "POST"
+                }
             }
-        }
-      }),
+        }),
     }),
-  })
+})
 
-  export const { useGetProductQuery } = productApi
+export const {useGetProductQuery} = productApi
 
